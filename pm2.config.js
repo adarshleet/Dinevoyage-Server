220e3075eaa900d05405b0dc728ce94b653ca29e
Adarsh <adarshravi011@gmail.com>
2023-12-21 11:48:09 +0530

    pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    script: 'dist/index.js', // Path to your compiled JavaScript file
+    watch: true, // Auto-restart the app when files change (optional)
+    ignore_watch: ['node_modules', 'logs'], // Ignore certain directories from auto-restart (optional)
+  };
