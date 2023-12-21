11fa613b8c26a064e4f3f98d6913bd0bd6b4cd14
Adarsh <adarshravi011@gmail.com>
2023-12-21 14:27:09 +0530

    twilio error

--- pm2.config.js
+++ pm2.config.js
@@ -2,4 +2,9 @@ module.exports = {
     script: 'dist/index.js', // Path to your compiled JavaScript file
     watch: true, // Auto-restart the app when files change (optional)
     ignore_watch: ['node_modules', 'logs'], // Ignore certain directories from auto-restart (optional)
+    env: {
+        ACC_SID : 'AC8d4bf4bf01f1bca9eddbe2dbd66abc7a',
+        AUTH_TOKEN : 'cc3f234f256fa5ef0b31f9a7b875178b',
+        SERVICE_SID : 'VA0368af8cae07819ac37e7e6fc5f82cd9'
+    },
   };
