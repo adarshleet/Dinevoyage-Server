4d5d753aa4eb2784f02bfe2f46d57497aa57a3f0
Adarsh <adarshravi011@gmail.com>
2023-12-22 10:26:08 +0530

    env problem fixing

--- pm2.config.js
+++ pm2.config.js
@@ -3,11 +3,11 @@ module.exports = {
     watch: true, // Auto-restart the app when files change (optional)
     ignore_watch: ['node_modules', 'logs'], // Ignore certain directories from auto-restart (optional)
     env: {
-        ACC_SID : 'AC8d4bf4bf01f1bca9eddbe2dbd66abc7a',
-        AUTH_TOKEN : 'cc3f234f256fa5ef0b31f9a7b875178b',
-        SERVICE_SID : 'VA0368af8cae07819ac37e7e6fc5f82cd9',
+        // ACC_SID : 'AC8d4bf4bf01f1bca9eddbe2dbd66abc7a',
+        // AUTH_TOKEN : 'cc3f234f256fa5ef0b31f9a7b875178b',
+        // SERVICE_SID : 'VA0368af8cae07819ac37e7e6fc5f82cd9',
         NODE_ENV: 'production',
         PORT: 3000,
-        MONGO_URI : "mongodb+srv://adarshravi:adarsh98475@dinevoyage.s4rqywm.mongodb.net/DineVoyage?retryWrites=true&w=majority"
+        // MONGO_URI : "mongodb+srv://adarshravi:adarsh98475@dinevoyage.s4rqywm.mongodb.net/DineVoyage?retryWrites=true&w=majority"
     },
-  };
+};
