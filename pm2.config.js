94e028a4f74676cbd675c5592f46c97418b33a4b
Adarsh <adarshravi011@gmail.com>
2023-12-24 10:45:51 +0530

    cloudinary api key

--- pm2.config.js
+++ pm2.config.js
@@ -1,15 +1,12 @@
 module.exports = {
     script: 'dist/index.js', // Path to your compiled JavaScript file
     watch: true, // Auto-restart the app when files change (optional)
-    ignore_watch: ['node_modules', 'logs'], // Ignore certain directories from auto-restart (optional)
+    ignore_watch: ['node_modules','logs'], // Ignore certain directories from auto-restart (optional)
     env: {
         NODE_ENV: 'production',
         PORT: 3000,
         CLOUDINARY_CLOUD : 'dfbyutwmz',
         CLOUDINARY_API_KEY : 616888345499255,
         CLOUDINARY_API_SECRET : 'n8GoExGOO1f4_Vp63f3KJoDC5CA',
-
-
-        STRIPE_API : 'sk_test_51OEOTHSFAVCVwY62x0N8IvUkwA0QkI6nQmji6nwfmLV9PaVNu2z575ruttZagRdBqeabY1IUC43GbxUEn3SogR4g00fnJRhx0T'
     },
 };
